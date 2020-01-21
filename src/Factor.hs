5c0f627302c6c44fc06abde3bbb790855ff9600c
John Sullivan <john.sullivan@rallyhealth.com>
2020-01-20 18:57:21 -0600

    really implement trial division
    i want to see if i can do that without recursion...

--- src/Factor.hs
+++ src/Factor.hs
@@ -1,16 +1,14 @@
 module Factor (factor) where
 
-import Data.List (find)
-
 factor :: Int -> [Int]
 factor n
   | n <= 0       = error "factor of a non-positive number"
   | n == 1       = []
   | mod n 2 == 0 = 2 : factor (div n 2)
-  | otherwise    =
-    let odds            = [3, 5 ..]
-        possibleFactors = takeWhile (\x -> x * x <= n) odds
-        smallestFactor  = find (\x -> mod n x == 0) possibleFactors 
-    in case smallestFactor of
-      Just f  -> f : factor (div n f)
-      Nothing -> [n]
+  | otherwise    = factorOdds n 3
+
+factorOdds :: Int -> Int -> [Int]
+factorOdds n odd
+  | n < odd * odd  = [n]
+  | mod n odd == 0 = odd : factorOdds (div n odd) odd
+  | otherwise      = factorOdds n (odd + 2)
